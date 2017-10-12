fb3e4086c63841f35f3b90e97ab446d3908a0dd6
phper <32724515+ouyangxing11@users.noreply.github.com>
2017-10-12 14:02:37 +0800

    time
    time

---
+++ time.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+	</head>
+	<body>
+		<?php
+			$str1="今天是";
+			$str2="星期二";
+			echo $str1.$str2;
+			echo '<br/>';
+			
+			echo date("Y-m-d:H i s T");
+			
+			//输出当天的日期
+			echo date("y-m-d",time());
+			
+			
+		?>
+	</body>
+</html>
